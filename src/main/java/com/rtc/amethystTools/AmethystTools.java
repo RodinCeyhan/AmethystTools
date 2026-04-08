de481b9bdf3cb9f54bdd150e60dc3af84e827664
RodinToprak <toprakceyhan2010@gmail.com>
2026-04-09 01:51:33 +0300

    26.1.X

--- src/main/java/com/rtc/amethystTools/AmethystTools.java
+++ src/main/java/com/rtc/amethystTools/AmethystTools.java
@@ -8,11 +8,22 @@ import com.rtc.amethystTools.listener.AmethystToolHoldSound;
 import com.rtc.amethystTools.listener.AnvilListener;
 import com.rtc.amethystTools.listener.GrindstoneListener;
 import com.rtc.amethystTools.menu.*;
+import com.rtc.amethystTools.utils.UpdateChecker;
+import net.kyori.adventure.text.Component;
+import net.kyori.adventure.text.event.ClickEvent;
+import net.kyori.adventure.text.event.HoverEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 import org.bukkit.NamespacedKey;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-@SuppressWarnings("unused")
-public final class AmethystTools extends JavaPlugin {
+@SuppressWarnings({"unused", "deprecation"})
+public final class AmethystTools extends JavaPlugin implements Listener {
+
+    public UpdateChecker updateChecker;
 
     private static AmethystTools instance;
 
@@ -31,6 +42,20 @@ public final class AmethystTools extends JavaPlugin {
         KEY_TOOL = new NamespacedKey(this, "amethyst_tool");
         KEY_TIER = new NamespacedKey(this, "amethyst_tier");
         KEY_TYPE = new NamespacedKey(this, "amethyst_type");
+
+        getServer().getPluginManager().registerEvents(this, this);
+
+        Bukkit.getConsoleSender().sendMessage("§d                          _   _               _ _______          _     ");
+        Bukkit.getConsoleSender().sendMessage("§d     /\\                  | | | |             | |__   __|        | |    ");
+        Bukkit.getConsoleSender().sendMessage("§d    /  \\   _ __ ___   ___| |_| |__  _   _ ___| |_ | | ___   ___ | |___ ");
+        Bukkit.getConsoleSender().sendMessage("§d   / /\\ \\ | '_ ` _ \\ / _ \\ __| '_ \\| | | / __| __|| |/ _ \\ / _ \\| / __|");
+        Bukkit.getConsoleSender().sendMessage("§d  / ____ \\| | | | | |  __/ |_| | | | |_| \\__ \\ |_ | | (_) | (_) | \\__ \\");
+        Bukkit.getConsoleSender().sendMessage("§d /_/    \\_\\_| |_| |_|\\___|\\__|_| |_|\\__, |___/\\__||_|\\___/ \\___/|_|___/");
+        Bukkit.getConsoleSender().sendMessage("§d                                     __/ |                             ");
+        Bukkit.getConsoleSender().sendMessage("§d                                    |___/                              ");
+
+        updateChecker = new UpdateChecker(this);
+        updateChecker.check();
 
         getServer().getPluginManager().registerEvents(new AmethystToolMainMenu(this), this);
 
@@ -51,5 +76,23 @@ public final class AmethystTools extends JavaPlugin {
 
         getServer().getPluginManager().registerEvents(new AnvilListener(), this);
         getServer().getPluginManager().registerEvents(new GrindstoneListener(), this);
+    }
+    @EventHandler
+    public void onJoinEvent(PlayerJoinEvent event) {
+        if (updateChecker.latestVersion != null && updateChecker.isUpdateAvailable(updateChecker.currentVersion, updateChecker.latestVersion)) {
+            if (event.getPlayer().isOp()) {
+                String prefix = "&7[&dAmethystTools&7]";
+
+                event.getPlayer().sendMessage(ChatColor.translateAlternateColorCodes('&',
+                        prefix + " " + "&aThere is a newer plugin version available: &n" + updateChecker.latestVersion + "&r&a, you're on: &n" + updateChecker.currentVersion
+                ));
+
+                Component updateLink = Component.text(ChatColor.translateAlternateColorCodes('&', prefix + " &aClick here to download the new version."))
+                        .clickEvent(ClickEvent.openUrl("https://modrinth.com/plugin/amethystools/versions"))
+                        .hoverEvent(HoverEvent.showText(Component.text(ChatColor.translateAlternateColorCodes('&', "&7Click to go to the Modrinth download page."))));
+
+                event.getPlayer().sendMessage(updateLink);
+            }
+        }
     }
 }
